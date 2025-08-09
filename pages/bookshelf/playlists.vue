ace02994872294e99bf4fb10c1fafd21ed053611
yougotborked <anbork@gmail.com>
2025-08-09 17:20:42 -0500

    fix: use server episode id for progress (#39)

--- pages/bookshelf/playlists.vue
+++ pages/bookshelf/playlists.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="w-full h-full overflow-y-auto">
-    <div class="px-4 pt-4" v-if="showAutoPlaylist">
-      <div class="mb-4 border border-fg/20 rounded p-4 flex items-center">
+    <div class="px-4 pt-4">
+      <div
+        v-if="autoCacheUnplayedEpisodes"
+        class="mb-4 border border-fg/20 rounded p-4 flex items-center"
+      >
         <nuxt-link to="/playlist/unfinished" class="flex items-center flex-grow">
           <covers-playlist-cover :items="autoPlaylist.items" :width="64" :height="64" />
           <p class="text-lg ml-4">{{ autoPlaylist.name }}</p>
@@ -16,8 +19,11 @@
 import { AbsDownloader } from '@/plugins/capacitor'
 export default {
   async asyncData({ store, app }) {
-    const cached = await app.$localStore.getCachedPlaylist('unfinished')
     const name = app.$strings.LabelAutoUnfinishedPodcasts
+    const enabled = store.state.deviceData?.deviceSettings?.autoCacheUnplayedEpisodes
+    if (!enabled)
+      return { autoPlaylist: { id: 'unfinished', name, items: [] } }
+    const cached = await app.$localStore.getCachedPlaylist('unfinished')
     if (cached) return { autoPlaylist: cached }
     return { autoPlaylist: { id: 'unfinished', name, items: [] } }
   },
@@ -27,12 +33,11 @@ export default {
     }
   },
   mounted() {
-    if (this.showAutoPlaylist) this.fetchAutoPlaylist()
+    if (this.autoCacheUnplayedEpisodes) this.fetchAutoPlaylist()
   },
   watch: {
     networkConnected(newVal) {
-      if (!this.showAutoPlaylist) return
-      if (newVal) {
+      if (newVal && this.autoCacheUnplayedEpisodes) {
         if (!this.autoPlaylist.items.length) {
           setTimeout(() => {
             this.fetchAutoPlaylist()
@@ -41,13 +46,18 @@ export default {
           this.checkAutoDownload()
         }
       }
+    },
+    autoCacheUnplayedEpisodes(newVal) {
+      if (newVal && !this.autoPlaylist.items.length) {
+        this.fetchAutoPlaylist()
+      }
     }
   },
   computed: {
     networkConnected() {
       return this.$store.state.networkConnected
     },
-    showAutoPlaylist() {
+    autoCacheUnplayedEpisodes() {
       return this.$store.state.deviceData?.deviceSettings?.autoCacheUnplayedEpisodes
     }
   },
@@ -115,8 +125,9 @@ export default {
           const key = `${li.libraryItemId}_${serverId}`
           if (seen.has(key)) continue
           seen.add(key)
+          const serverLibraryItem = { ...li, id: li.libraryItemId }
           items.push({
-            libraryItem: li,
+            libraryItem: serverLibraryItem,
             episode: ep,
             libraryItemId: li.libraryItemId,
             episodeId: serverId,
