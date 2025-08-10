93a001dfe9fe712c46e97ff3762c724311c85e37
yougotborked <anbork@gmail.com>
2025-08-10 17:44:46 -0500

    fix: allow offline access to unfinished playlist

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -3,6 +3,7 @@ export default function ({ store, redirect, route }) {
   const serverConfig = store.state.user.serverConnectionConfig
 
   if (!user && !serverConfig) {
+    if (route.name === 'playlist-id' && route.params.id === 'unfinished') return
     return redirect(`/connect?redirect=${route.path}`)
   }
 }
