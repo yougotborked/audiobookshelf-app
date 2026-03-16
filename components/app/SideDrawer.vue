ca0b316c672e120b900d5326abcaa2e2951f9e3e
Andrew Bork <abork@abork.co>
2026-03-16 18:00:16 -0500

    feat: port Appbar.vue and SideDrawer.vue to Vue 3 script setup lang ts

--- components/app/SideDrawer.vue
+++ components/app/SideDrawer.vue
@@ -3,7 +3,7 @@
     <div class="absolute top-0 left-0 w-full h-full bg-black transition-opacity duration-200" :class="show ? 'bg-opacity-60 pointer-events-auto' : 'bg-opacity-0'" @click="clickBackground" />
     <div class="absolute top-0 right-0 w-64 h-full bg-md-surface-1 transform transition-transform py-6 landscape:py-2 pointer-events-auto" :class="show ? '' : 'translate-x-64'" @click.stop>
       <div class="px-6 mb-4">
-        <p v-if="user" class="text-base" v-html="$getString('HeaderWelcome', [username])" />
+        <p v-if="user" class="text-base" v-html="getString('HeaderWelcome', [username])" />
       </div>
 
       <div class="w-full overflow-y-auto" style="max-height: calc(100vh - 180px)">
@@ -12,7 +12,7 @@
             <span class="material-symbols fill text-lg">{{ item.icon }}</span>
             <p class="pl-4">{{ item.text }}</p>
           </button>
-          <nuxt-link v-else :to="item.to" :key="item.text" :tabindex="show ? 0 : -1" class="w-full hover:bg-md-surface-1/60 flex items-center py-3 px-6 text-md-on-surface" :class="currentRoutePath.startsWith(item.to) ? 'bg-md-on-surface/5' : 'text-md-on-surface-variant'">
+          <nuxt-link v-else :to="item.to" :key="item.text" :tabindex="show ? 0 : -1" class="w-full hover:bg-md-surface-1/60 flex items-center py-3 px-6 text-md-on-surface" :class="item.to && currentRoutePath.startsWith(item.to) ? 'bg-md-on-surface/5' : 'text-md-on-surface-variant'">
             <span class="material-symbols fill text-lg">{{ item.icon }}</span>
             <p class="pl-4">{{ item.text }}</p>
           </nuxt-link>
@@ -23,10 +23,10 @@
           <p class="text-xs text-md-on-surface-variant" style="word-break: break-word">{{ serverConnectionConfig.address }} (v{{ serverSettings.version }})</p>
         </div>
         <div class="flex items-center">
-          <p class="text-xs">{{ $config.version }}</p>
+          <p class="text-xs">{{ config.public.version }}</p>
           <div class="flex-grow" />
           <div v-if="user" class="flex items-center" @click="disconnect">
-            <p class="text-xs pr-2">{{ $strings.ButtonDisconnect }}</p>
+            <p class="text-xs pr-2">{{ getStrings().ButtonDisconnect }}</p>
             <i class="material-symbols text-sm -mb-0.5">cloud_off</i>
           </div>
         </div>
@@ -35,187 +35,124 @@
   </div>
 </template>
 
-<script>
-import TouchEvent from '@/objects/TouchEvent'
+<script setup lang="ts">
+import AppTouchEvent from '@/objects/TouchEvent'
+import { getString } from '~/composables/useStrings'
 
-export default {
-  data() {
-    return {
-      touchEvent: null
-    }
-  },
-  watch: {
-    $route: {
-      handler() {
-        this.show = false
-      }
-    },
-    show: {
-      handler(newVal) {
-        if (newVal) this.registerListener()
-        else this.removeListener()
-      }
-    }
-  },
-  computed: {
-    show: {
-      get() {
-        return this.$store.state.showSideDrawer
-      },
-      set(val) {
-        this.$store.commit('setShowSideDrawer', val)
-      }
-    },
-    user() {
-      return this.$store.state.user.user
-    },
-    serverConnectionConfig() {
-      return this.$store.state.user.serverConnectionConfig
-    },
-    serverSettings() {
-      return this.$store.state.serverSettings || {}
-    },
-    username() {
-      return this.user?.username || ''
-    },
-    userIsAdminOrUp() {
-      return this.$store.getters['user/getIsAdminOrUp']
-    },
-    navItems() {
-      var items = [
-        {
-          icon: 'home',
-          text: this.$strings.ButtonHome,
-          to: '/bookshelf'
-        }
-      ]
-      if (!this.serverConnectionConfig) {
-        items = [
-          {
-            icon: 'cloud_off',
-            text: this.$strings.ButtonConnectToServer,
-            to: '/connect'
-          }
-        ].concat(items)
-      } else {
-        items.push({
-          icon: 'person',
-          text: this.$strings.HeaderAccount,
-          to: '/account'
-        })
-        items.push({
-          icon: 'equalizer',
-          text: this.$strings.ButtonUserStats,
-          to: '/stats'
-        })
-      }
+const appStore = useAppStore()
+const userStore = useUserStore()
+const librariesStore = useLibrariesStore()
+const route = useRoute()
+const router = useRouter()
+const config = useRuntimeConfig()
+const bus = useEventBus()
+const getStrings = () => useStrings()
+const { impact: hapticsImpact } = useHaptics()
+const platform = usePlatform()
 
-      if (this.$platform !== 'ios') {
-        items.push({
-          icon: 'folder',
-          iconOutlined: true,
-          text: this.$strings.ButtonLocalMedia,
-          to: '/localMedia/folders'
-        })
-      } else {
-        items.push({
-          icon: 'download',
-          iconOutlined: false,
-          text: this.$strings.HeaderDownloads,
-          to: '/downloads'
-        })
-      }
-      items.push({
-        icon: 'settings',
-        text: this.$strings.HeaderSettings,
-        to: '/settings'
-      })
+const touchEventRef = ref<InstanceType<typeof AppTouchEvent> | null>(null)
 
-      items.push({
-        icon: 'bug_report',
-        iconOutlined: true,
-        text: this.$strings.ButtonLogs,
-        to: '/logs'
-      })
+const show = computed({
+  get: () => appStore.showSideDrawer,
+  set: (val: boolean) => { appStore.showSideDrawer = val }
+})
 
-      if (this.serverConnectionConfig) {
-        items.push({
-          icon: 'language',
-          text: this.$strings.ButtonGoToWebClient,
-          action: 'openWebClient'
-        })
+const user = computed(() => userStore.user)
+const serverConnectionConfig = computed(() => userStore.serverConnectionConfig)
+const serverSettings = computed(() => appStore.serverSettings || {})
+const username = computed(() => (user.value as Record<string, unknown>)?.username as string || '')
 
-        items.push({
-          icon: 'login',
-          text: this.$strings.ButtonSwitchServerUser,
-          action: 'logout'
-        })
-      }
+const navItems = computed(() => {
+  const items: { icon: string; text: string; to?: string; action?: string; iconOutlined?: boolean }[] = [
+    { icon: 'home', text: getStrings().ButtonHome, to: '/bookshelf' }
+  ]
+  if (!serverConnectionConfig.value) {
+    items.unshift({ icon: 'cloud_off', text: getStrings().ButtonConnectToServer, to: '/connect' })
+  } else {
+    items.push({ icon: 'person', text: getStrings().HeaderAccount, to: '/account' })
+    items.push({ icon: 'equalizer', text: getStrings().ButtonUserStats, to: '/stats' })
+  }
 
-      return items
-    },
-    currentRoutePath() {
-      return this.$route.path
-    }
-  },
-  methods: {
-    async clickAction(action) {
-      await this.$hapticsImpact()
-      if (action === 'logout') {
-        await this.logout()
-        this.$router.push('/connect')
-      } else if (action === 'openWebClient') {
-        this.show = false
-        let path = `/library/${this.$store.state.libraries.currentLibraryId}`
-        await this.$store.dispatch('user/openWebClient', path)
-      }
-    },
-    clickBackground() {
-      this.show = false
-    },
-    async logout() {
-      await this.$store.dispatch('user/logout')
-    },
-    async disconnect() {
-      await this.$hapticsImpact()
-      await this.logout()
+  if (platform !== 'ios') {
+    items.push({ icon: 'folder', iconOutlined: true, text: getStrings().ButtonLocalMedia, to: '/localMedia/folders' })
+  } else {
+    items.push({ icon: 'download', iconOutlined: false, text: getStrings().HeaderDownloads, to: '/downloads' })
+  }
+  items.push({ icon: 'settings', text: getStrings().HeaderSettings, to: '/settings' })
+  items.push({ icon: 'bug_report', iconOutlined: true, text: getStrings().ButtonLogs, to: '/logs' })
 
-      // Redirect to home page
-      if (this.$route.name !== 'bookshelf') {
-        this.$router.replace('/bookshelf')
-      }
+  if (serverConnectionConfig.value) {
+    items.push({ icon: 'language', text: getStrings().ButtonGoToWebClient, action: 'openWebClient' })
+    items.push({ icon: 'login', text: getStrings().ButtonSwitchServerUser, action: 'logout' })
+  }
 
-      // If player is open and not playing locally, then close the player
-      if (this.$store.getters['getIsPlayerOpen']) {
-        this.$eventBus.$emit('close-stream')
-      }
+  return items
+})
 
-      // Close side drawer
-      this.show = false
-    },
-    touchstart(e) {
-      this.touchEvent = new TouchEvent(e)
-    },
-    touchend(e) {
-      if (!this.touchEvent) return
-      this.touchEvent.setEndEvent(e)
-      if (this.touchEvent.isSwipeRight()) {
-        this.show = false
-      }
-      this.touchEvent = null
-    },
-    registerListener() {
-      document.addEventListener('touchstart', this.touchstart)
-      document.addEventListener('touchend', this.touchend)
-    },
-    removeListener() {
-      document.removeEventListener('touchstart', this.touchstart)
-      document.removeEventListener('touchend', this.touchend)
-    }
-  },
-  mounted() {},
-  beforeDestroy() {
-    this.show = false
+const currentRoutePath = computed(() => route.path)
+
+async function clickAction(action: string) {
+  await hapticsImpact()
+  if (action === 'logout') {
+    await userStore.logout()
+    router.push('/connect')
+  } else if (action === 'openWebClient') {
+    show.value = false
+    const path = `/library/${librariesStore.currentLibraryId}`
+    await userStore.openWebClient(path)
   }
 }
+
+function clickBackground() {
+  show.value = false
+}
+
+async function disconnect() {
+  await hapticsImpact()
+  await userStore.logout()
+
+  if (route.name !== 'bookshelf') {
+    router.replace('/bookshelf')
+  }
+
+  if (appStore.getIsPlayerOpen) {
+    bus.emit('close-stream')
+  }
+
+  show.value = false
+}
+
+function touchstart(e: Event) {
+  touchEventRef.value = new AppTouchEvent(e)
+}
+
+function touchend(e: Event) {
+  if (!touchEventRef.value) return
+  touchEventRef.value.setEndEvent(e)
+  if (touchEventRef.value.isSwipeRight()) {
+    show.value = false
+  }
+  touchEventRef.value = null
+}
+
+function registerListener() {
+  document.addEventListener('touchstart', touchstart)
+  document.addEventListener('touchend', touchend)
+}
+
+function removeListener() {
+  document.removeEventListener('touchstart', touchstart as EventListener)
+  document.removeEventListener('touchend', touchend as EventListener)
+}
+
+watch(route, () => { show.value = false })
+watch(show, (newVal) => {
+  if (newVal) registerListener()
+  else removeListener()
+})
+
+onBeforeUnmount(() => {
+  show.value = false
+  removeListener()
+})
 </script>
